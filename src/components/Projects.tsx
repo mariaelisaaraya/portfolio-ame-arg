39afebaba2f22100f4b10e5777fabc190f69fd2f
Maria Elisa Araya <42949801+mariaelisaaraya@users.noreply.github.com>
2025-02-14 15:01:32 -0300

    update vercel

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -19,7 +19,7 @@ const projects = [
   {
     category: "Next - Figma - UX UI",
     title: "DefiWise 2024",
-    image: "images/defiWise1.png",
+    image: "src/public/defiwise1.png",
     path: "/project/defiwise",
   },
   {
