b21bd9ef2088cb1522ad5e72921b0c2d373ce0c0
Maria Elisa Araya <42949801+mariaelisaaraya@users.noreply.github.com>
2025-02-14 13:47:18 -0300

    add app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import { RoutesConfig } from "./components/RoutesConfig";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <RoutesConfig />
+    </BrowserRouter>
+  );
+}
+
+export default App;
